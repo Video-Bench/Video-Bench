bf3a929c5e21c2a7d34c0e174b88539894ae78f0
dragntigr <a15942392581@163.com>
2025-06-03 13:59:13 +0800

    update

--- setup.py
+++ setup.py
@@ -29,6 +29,5 @@ setup(name='videobench',
       install_requires=install_requires,
       packages=find_packages(),
       include_package_data=True,
-      license='Apache-2.0',
       python_requires='>=3.8',
 )
